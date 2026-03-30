7839afefd27f6dc2009de795c52cf957042f0e40
Kjell4 <ilias.bekturgan@mail.ru>
2026-03-30 20:46:53 +0500

    postgres changes

--- esg-report/backend/settings.py
+++ esg-report/backend/settings.py
@@ -88,13 +88,20 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'esg_database',
-        'USER': 'myuser',
-        'PASSWORD': 'mypassword',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'NAME': 'esg_database',       
+        'USER': 'myuser',           
+        'PASSWORD': 'mypassword',      
+        'HOST': 'localhost',          
+        'PORT': '5432',               
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 
 # Password validation
